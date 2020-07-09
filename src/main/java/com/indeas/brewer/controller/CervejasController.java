b83751b260281d42b299a2722899461c6a2169ec
Alexsandro de Lima Silva <alexlima_cti@hotmail.com>
2020-07-09 17:36:54 -0300

    inicio do projeto

---
+++ src/main/java/com/indeas/brewer/controller/CervejasController.java
@@ -0,0 +1,27 @@
+package com.indeas.brewer.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.indeas.brewer.model.Cerveja;
+
+@Controller
+public class CervejasController {
+
+    @RequestMapping("/cervejas/novo")
+    public String novo() {
+        return "cerveja/CadastroCerveja";
+    }
+
+    @RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+    public String cadastrar(@Valid Cerveja cerveja, BindingResult result) {
+    	if(result.hasErrors())
+    		System.out.println(">>>> Tem erro sim");
+        System.out.println(">>>>> SKU: " + cerveja.getSku());
+        return "cerveja/CadastroCerveja";
+    }
+}
