e740284448c91aa6c955c3b6c850f86cc7306eaa
Alexsandro de Lima Silva <alexlima_cti@hotmail.com>
2020-10-09 08:38:28 -0300

    ajustes no cadastro de estilo

--- src/main/java/com/indeas/brewer/model/Estilo.java
+++ src/main/java/com/indeas/brewer/model/Estilo.java
@@ -9,6 +9,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "estilo")
@@ -20,8 +22,10 @@ public class Estilo implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 
+	@NotBlank(message = "O nome é obrigatório")
+	@Size(max = 20, message = "O tamanho do nome não pode ser maior que {max} caracteres")
 	private String nome;
-	
+
 	@OneToMany(mappedBy = "estilo")
 	private List<Cerveja> cervejas;
 
---
+++ src/main/java/com/indeas/brewer/service/CadastroEstiloService.java
@@ -0,0 +1,28 @@
+package com.indeas.brewer.service;
+
+import java.util.Optional;
+
+import com.indeas.brewer.exception.NomeEstiloJaCadastradoException;
+import com.indeas.brewer.model.Estilo;
+import com.indeas.brewer.repository.Estilos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CadastroEstiloService {
+
+	@Autowired
+	private Estilos estilos;
+	
+	@Transactional
+	public Estilo salvar(Estilo estilo) {
+		Optional<Estilo> estiloOptional = estilos.findByNomeIgnoreCase(estilo.getNome());
+		if (estiloOptional.isPresent()) {
+			throw new NomeEstiloJaCadastradoException("Nome do estilo já cadastrado");
+		}
+		
+		return estilos.saveAndFlush(estilo);
+	}
+	
+}
