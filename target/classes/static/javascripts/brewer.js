e298fdea982649432f493c7ce3803d687e549b09
Alexsandro <alexlima_cti@hotmail.com>
2021-02-11 11:48:25 -0300

    ajustes vendas autocomplete

--- target/classes/static/javascripts/brewer.js
+++ target/classes/static/javascripts/brewer.js
@@ -69,6 +69,11 @@ Brewer.Security = (function() {
 	return Security;
 }());
 
+Brewer.formatarMoeda = function(valor) {
+	numeral.language('pt-br');
+	return numeral(valor).format('0,0.00');
+}
+
 $(function() {
 	var maskMoney = new Brewer.MaskMoney();
 	var maskPhoneNumber = new Brewer.MaskPhoneNumber();
