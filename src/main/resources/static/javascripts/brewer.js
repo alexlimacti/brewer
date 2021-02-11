f352d7861b2c29d2764aada6aed3a6e3394d5f5b
Alexsandro <alexlima_cti@hotmail.com>
2021-02-11 16:50:37 -0300

    correcao autocomplete cervejas vendas

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -1,49 +1,65 @@
 var Brewer = Brewer || {};
 
 Brewer.MaskMoney = (function() {
+
 	function MaskMoney() {
 		this.decimal = $('.js-decimal');
 		this.plain = $('.js-plain');
 	}
+
 	MaskMoney.prototype.enable = function() {
 		this.decimal.maskMoney({ decimal: ',', thousands: '.' });
 		this.plain.maskMoney({ precision: 0, thousands: '.' });
 	}
+
 	return MaskMoney;
+
 }());
 
 Brewer.MaskPhoneNumber = (function() {
+
 	function MaskPhoneNumber() {
 		this.inputPhoneNumber = $('.js-phone-number');
 	}
+
 	MaskPhoneNumber.prototype.enable = function() {
 		var maskBehavior = function (val) {
-		  return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+			return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
 		};
+
 		var options = {
-		  onKeyPress: function(val, e, field, options) {
-		      field.mask(maskBehavior.apply({}, arguments), options);
-		    }
+			onKeyPress: function(val, e, field, options) {
+				field.mask(maskBehavior.apply({}, arguments), options);
+			}
 		};
+
 		this.inputPhoneNumber.mask(maskBehavior, options);
 	}
+
 	return MaskPhoneNumber;
+
 }());
 
 Brewer.MaskCep = (function() {
+
 	function MaskCep() {
 		this.inputCep = $('.js-cep');
 	}
+
 	MaskCep.prototype.enable = function() {
 		this.inputCep.mask('00.000-000');
 	}
+
 	return MaskCep;
+
 }());
 
 Brewer.MaskDate = (function() {
+
 	function MaskDate() {
 		this.inputDate = $('.js-date');
 	}
+
 	MaskDate.prototype.enable = function() {
 		this.inputDate.mask('00/00/0000');
 		this.inputDate.datepicker({
@@ -52,25 +68,30 @@ Brewer.MaskDate = (function() {
 			autoclose: true
 		});
 	}
+
 	return MaskDate;
+
 }());
 
-//Segurança no Ajax
 Brewer.Security = (function() {
+
 	function Security() {
 		this.token = $('input[name=_csrf]').val();
 		this.header = $('input[name=_csrf_header]').val();
 	}
+
 	Security.prototype.enable = function() {
 		$(document).ajaxSend(function(event, jqxhr, settings) {
 			jqxhr.setRequestHeader(this.header, this.token);
 		}.bind(this));
 	}
+
 	return Security;
+
 }());
 
 Brewer.formatarMoeda = function(valor) {
-	numeral.language('pt-br');
+	numeral.locale('pt-br');
 	return numeral(valor).format('0,0.00');
 }
 
