13e48158f5266e20320462b619001eee0808a37f
Alexsandro Lima Silva <alexsandrolima.dev@gmail.com>
2022-07-29 09:27:42 -0300

    Teste Git

--- src/main/java/com/indeas/brewer/repository/Cervejas.java
+++ src/main/java/com/indeas/brewer/repository/Cervejas.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Cervejas extends JpaRepository<Cerveja, Long>, CervejasQueries {
+public interface Cervejas extends JpaRepository<Cerveja, Long>, CervejasQueries  {
 
 }
